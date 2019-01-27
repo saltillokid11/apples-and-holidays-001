21da2cc67c964a1c110bdaaabfdd173e93cd8fe9
Tanner Lancaster <tanner@flatironschool.com>
2019-01-27 18:45:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,9 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   arr = []
   holiday_hash[:winter].each do |holiday, a|
-    arr << a
+    a.each do |i|
+      arr << i
+    end
   end
   arr.flatten
   p arr
