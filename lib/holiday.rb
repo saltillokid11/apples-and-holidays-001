89c74c57b080e5278c39c1d7abafa31b14d67a7f
Tanner Lancaster <tanner@flatironschool.com>
2019-01-27 18:13:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,6 +50,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   p holiday_hash
+  puts "----------------------"
   holiday_hash[season][holiday_name] = supply_array
   p holiday_hash
 end
