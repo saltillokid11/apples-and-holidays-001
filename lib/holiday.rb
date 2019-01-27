36088b6d75556898d0eb955c55b6fdd6493929f9
Tanner Lancaster <tanner@flatironschool.com>
2019-01-27 17:44:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -24,7 +24,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  holiday_hash[:winter][:fourth_of_july] << "BBQ"
+  holiday_hash[:winter][:christmas] << "BBQ"
+  holiday_hash[:winter][:new_years] << "BBQ"
 end
 
 
