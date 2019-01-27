a2fc0d142e7f49198512ba1d48f7aff64eb3e42f
Tanner Lancaster <tanner@flatironschool.com>
2019-01-27 19:48:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,9 +75,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     binding.pry
-    # holiday.each do |hol, sup|
-    #   puts "#{hol.to_s}:" + " " + "#{sup}"
-    # end
+    holiday.each do |hol, sup|
+      puts "#{hol.to_s}:" + " " + "#{sup}"
+    end
   end
 
 end
