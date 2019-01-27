cd6f8347cd464e7424c6319d715835b1c1c820e8
Tanner Lancaster <tanner@flatironschool.com>
2019-01-27 18:03:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,7 +30,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :winter
       holiday.each do |s|
-        # p h
         s << supply
         p s
       end
