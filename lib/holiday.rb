5f9a2d82e96fdf51c63368200224aba93ad913ed
Tanner Lancaster <tanner@flatironschool.com>
2019-01-27 20:10:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,5 +84,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   arr = [];
-  
+  holiday_hash.each do |sea, hol|
+    
+  end
 end
