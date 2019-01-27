9381abd8c3d9f47855e9a1495413c986f0e3b2fb
Tanner Lancaster <tanner@flatironschool.com>
2019-01-27 18:06:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday|
     if season == :winter
-      holiday.each do |s|
+      holiday.each do |holiday, supplies|
         s << supply
         s.flatten
         p s
