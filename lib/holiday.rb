40d89d139a3c42db799992a94458be0b86062dd0
Tanner Lancaster <tanner@flatironschool.com>
2019-01-27 17:50:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,8 +18,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_hash[:summer][:fourth_of_july] << "BBQ"
-  p holiday_hash[:summer][:fourth_of_july]
+  # holiday_hash[:summer][:fourth_of_july] << "BBQ"
+  p holiday_hash[:summer][:fourth_of_july][1]
   # return holiday_hash[:summer][:fourth_of_july][2]
 end
 
